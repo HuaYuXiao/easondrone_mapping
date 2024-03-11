909b4e18e2a6f821eb0d38b28e27d488f85d33da
Eason Hua <hyx020222@gmail.com>
2024-03-11 13:44:38 +0800

    Upload files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amov/catkin_ws/src/octomapping
-# Build directory: /home/amov/catkin_ws/src/octomapping/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/amov/catkin_ws/src/octomapping/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
