37533e62d9f4f1b3ac1f96ab41d9f0d68b34dc4e
新一 <473867143@qq.com>
2024-03-11 19:37:50 +0800

    test

---
+++ build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/amov/catkin_ws/src/octomapping/include".split(';') if "/home/amov/catkin_ws/src/octomapping/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lGlobal_Planning".split(';') if "-lGlobal_Planning" != "" else []
+PROJECT_NAME = "octomapping"
+PROJECT_SPACE_DIR = "/home/amov/catkin_ws/src/octomapping/build/devel"
+PROJECT_VERSION = "0.4.2"
