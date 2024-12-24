3b10f35e278f52ea071e49096e1ca17f70df7364
HuaYuXiao <hyx020222@gmail.com>
2024-12-24 16:58:41 +0800

    (beta)[upgrade] apply multithread to process several pointclouds parallely

--- src/merge_pcl/merge_pcl.cpp
+++ src/merge_pcl/merge_pcl.cpp
@@ -2,75 +2,97 @@
 
 using namespace std;
 
-PointCloudMerger::PointCloudMerger() : nh("~"){
+PointCloudMerger::PointCloudMerger() : nh("~") {
     nh.param<int>("pcl2_source_num", pcl2_source_num, 2);
-    nh.param<string>("pcl2_topic_0", pcl2_topic_0, "");
-    nh.param<string>("pcl2_frame_0", pcl2_frame_0, "");
-    nh.param<string>("pcl2_topic_1", pcl2_topic_1, "");
-    nh.param<string>("pcl2_frame_1", pcl2_frame_1, "");
-    nh.param<double>("tf_duration", tf_duration, 0.1);
-    nh.param<string>("pcl2_topic_out", pcl2_topic_out, "");
-    nh.param<string>("pcl2_frame_out", pcl2_frame_out, "");
-
     queue_size = boost::thread::hardware_concurrency();
 
-    pcl2_sub_0.reset(new message_filters::Subscriber<sensor_msgs::PointCloud2>
-    (nh, pcl2_topic_0, queue_size));
-    pcl2_sub_1.reset(new message_filters::Subscriber<sensor_msgs::PointCloud2>
-    (nh, pcl2_topic_1, queue_size));
+    nh.param<string>("pcl2_topic_0", pcl2_topic_0, "");
+    nh.param<string>("pcl2_frame_0", pcl2_frame_0, "");
+    pcl2_sub_0 = nh.subscribe(pcl2_topic_0, queue_size, &PointCloudMerger::pcl2Callback0, this);
 
-    synchronizer_.reset(new message_filters::Synchronizer<SyncPolicy>(SyncPolicy(100), *pcl2_sub_0, *pcl2_sub_1));
-    synchronizer_->registerCallback(boost::bind(&PointCloudMerger::mergeCallback, this, _1, _2));
+    nh.param<string>("pcl2_topic_1", pcl2_topic_1, "");
+    nh.param<string>("pcl2_frame_1", pcl2_frame_1, "");
+    pcl2_sub_1 = nh.subscribe(pcl2_topic_1, queue_size, &PointCloudMerger::pcl2Callback1, this);
 
-    pcl2_pub = nh.advertise<sensor_msgs::PointCloud2>(pcl2_topic_out, queue_size);
-
+    nh.param<double>("tf_duration", tf_duration, 0.1);
     listener0.waitForTransform(pcl2_frame_out, pcl2_frame_0, ros::Time(0), ros::Duration(tf_duration));
     listener1.waitForTransform(pcl2_frame_out, pcl2_frame_1, ros::Time(0), ros::Duration(tf_duration));
 
-    ROS_INFO("[easondrone_mapping] merge_pcl initialized!");
+    nh.param<string>("pcl2_topic_out", pcl2_topic_out, "");
+    nh.param<string>("pcl2_frame_out", pcl2_frame_out, "");
+    pcl2_pub = nh.advertise<sensor_msgs::PointCloud2>(pcl2_topic_out, queue_size);
+
+    ROS_INFO("[merge_pcl] Initialized!");
 }
 
-void PointCloudMerger::mergeCallback(const sensor_msgs::PointCloud2ConstPtr& pcl2_0, const sensor_msgs::PointCloud2ConstPtr& pcl2_1) {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pclxyz_0, pclxyz_1, pclxyz_output;
-
-    pclxyz_0.reset(new pcl::PointCloud<pcl::PointXYZ>());
-    pcl::fromROSMsg(*pcl2_0, *pclxyz_0);
-    // check if both frames are the same
-    if(pcl2_0->header.frame_id != pcl2_frame_out){
-        try{
-            pcl_ros::transformPointCloud(pcl2_frame_out, *pclxyz_0, *pclxyz_0, listener0);  
-        }
-        catch(...){
-            ROS_ERROR("pcl_ros::transformPointCloud failed!");
-        }
+void PointCloudMerger::pcl2Callback0(const sensor_msgs::PointCloud2ConstPtr& pcl2) {
+    std::lock_guard<std::mutex> lock(buffer_mutex);
+    buffer_0.push_back(pcl2);
+    if (buffer_0.size() > queue_size) {
+        buffer_0.pop_front();
     }
+}
 
-    pclxyz_1.reset(new pcl::PointCloud<pcl::PointXYZ>());
-    pcl::fromROSMsg(*pcl2_1, *pclxyz_1);
-    // check if both frames are the same
-    if(pcl2_1->header.frame_id != pcl2_frame_out){
-        try{
-            pcl_ros::transformPointCloud(pcl2_frame_out, *pclxyz_1, *pclxyz_1, listener1);  
-        }
-        catch(...){
-            ROS_ERROR("pcl_ros::transformPointCloud failed!");
-        }
+void PointCloudMerger::pcl2Callback1(const sensor_msgs::PointCloud2ConstPtr& pcl2) {
+    std::lock_guard<std::mutex> lock(buffer_mutex);
+    buffer_1.push_back(pcl2);
+    if (buffer_1.size() > queue_size) {
+        buffer_1.pop_front();
     }
+}
 
-    pclxyz_output.reset(new pcl::PointCloud<pcl::PointXYZ>());
+void PointCloudMerger::processBuffers() {
+    while (ros::ok()) {
+        sensor_msgs::PointCloud2ConstPtr pcl2_0 = nullptr;
+        sensor_msgs::PointCloud2ConstPtr pcl2_1 = nullptr;
 
-    pclxyz_output->header.seq = pclxyz_1->header.seq;
-    pclxyz_output->header.stamp = pclxyz_1->header.stamp;
-    pclxyz_output->header.frame_id = pcl2_frame_out;
+        {
+            std::lock_guard<std::mutex> lock(buffer_mutex);
+            if (!buffer_0.empty() && !buffer_1.empty()) {
+                pcl2_0 = buffer_0.front();
+                pcl2_1 = buffer_1.front();
+                buffer_0.pop_front();
+                buffer_1.pop_front();
+            }
+        }
 
-    *pclxyz_output += *pclxyz_1;
-    *pclxyz_output += *pclxyz_0;
+        if (pcl2_0 && pcl2_1) {
+            pcl::PointCloud<pcl::PointXYZ>::Ptr pclxyz_0, pclxyz_1, pclxyz_output;
 
-    sensor_msgs::PointCloud2 pcl2_output;
-    pcl::toROSMsg(*pclxyz_output, pcl2_output);
+            pclxyz_0.reset(new pcl::PointCloud<pcl::PointXYZ>());
+            pcl::fromROSMsg(*pcl2_0, *pclxyz_0);
+            if (pcl2_0->header.frame_id != pcl2_frame_out) {
+                try {
+                    pcl_ros::transformPointCloud(pcl2_frame_out, *pclxyz_0, *pclxyz_0, listener0);
+                } catch (...) {
+                    ROS_ERROR("Transform failed for pcl2_0!");
+                    continue;
+                }
+            }
 
-    if(pcl2_pub){
-        pcl2_pub.publish(pcl2_output);
+            pclxyz_1.reset(new pcl::PointCloud<pcl::PointXYZ>());
+            pcl::fromROSMsg(*pcl2_1, *pclxyz_1);
+            if (pcl2_1->header.frame_id != pcl2_frame_out) {
+                try {
+                    pcl_ros::transformPointCloud(pcl2_frame_out, *pclxyz_1, *pclxyz_1, listener1);
+                } catch (...) {
+                    ROS_ERROR("Transform failed for pcl2_1!");
+                    continue;
+                }
+            }
+
+            pclxyz_output.reset(new pcl::PointCloud<pcl::PointXYZ>());
+            *pclxyz_output += *pclxyz_0;
+            *pclxyz_output += *pclxyz_1;
+
+            sensor_msgs::PointCloud2 pcl2_output;
+            pcl::toROSMsg(*pclxyz_output, pcl2_output);
+
+            pcl2_output.header.frame_id = pcl2_frame_out;
+            pcl2_pub.publish(pcl2_output);
+        }
+
+        ros::Duration(0.01).sleep();  // Small delay to avoid excessive CPU usage
     }
 }
 
@@ -78,6 +100,10 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "merge_pcl");
     PointCloudMerger pc_merger;
 
+    boost::thread processing_thread(&PointCloudMerger::processBuffers, &pc_merger);
+
     ros::spin();
+    processing_thread.join();
+
     return 0;
 }
